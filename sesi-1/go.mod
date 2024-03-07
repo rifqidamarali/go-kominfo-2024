0fe22fc063f8014b7a3d3441c8a74c20208a7c20
Rifqi Damar Ali <rifqidamarali@gmail.com>
2024-03-07 15:47:49 +0700

    push variables kurang datatypes

---
+++ sesi-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/rifqidamarali/go-kominfo-2024/sesi-1
+
+go 1.21.3
---
+++ sesi-1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main (){
+	// varDeclaration()
+
+	
+}
+
+func varDeclaration(){
+	// define variable with data type
+	var name string
+	name = "Rifqi Damar Ali"
+	var number int = 10
+	fmt.Printf("Hi my name: %s my number is %d\n", name, number)
+
+	// define variable without data type
+
+	var car = "Honda"
+	var x = 8
+	fmt.Printf("car data type: %T x data type %T\n", car, x)
+
+
+	// short declartion
+
+	laptop := "lenovo"
+	fmt.Printf("merk laptop: %s", laptop)
+}
+
+func dataTypes(){
+
+}
